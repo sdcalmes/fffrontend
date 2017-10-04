a6713cb09b960fc69d2a673a603b9752c52ebedf
sdcalmes <scalmes@wisc.edu>
2017-10-04 08:42:26 -0500

    Update server.js

--- credentials/server.js
+++ credentials/server.js
@@ -1,9 +1,6 @@
-var admin = require("firebase-admin")
-var serviceAccount = require("./fantasyFiveServiceAcct.json");
+
 
 module.export = {
 	//todo server config
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://the-fantasy-five.firebaseio.com"
 
 }
