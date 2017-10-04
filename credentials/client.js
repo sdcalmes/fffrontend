7cb43cf567b3cd6fae2714eeb06b268c03ea928e
sdcalmes <scalmes@wisc.edu>
2017-10-04 08:42:09 -0500

    Update client.js

--- credentials/client.js
+++ credentials/client.js
@@ -1,9 +1,4 @@
 module.exports = {
-    apiKey: "AIzaSyDpx-L7txSEhlvGmb6TXt8xzi4DCUfHavU",
-    authDomain: "the-fantasy-five.firebaseapp.com",
-    databaseURL: "https://the-fantasy-five.firebaseio.com",
-    projectId: "the-fantasy-five",
-    storageBucket: "the-fantasy-five.appspot.com",
-    messagingSenderId: "980025347804"
+
 	//todo client config
 }
